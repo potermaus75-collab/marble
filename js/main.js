7f9c7aba7b8b0fca499434c6d1169d230c258fcb
potermaus75-collab <potermaus75@gmail.com>
2026-02-02 10:10:41 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
+import Board from './Board.js';
+import Game from './Game.js';
 
+window.onload = () => {
+    const board = new Board();
+    const game = new Game(board);
+    
+    // 모바일 주소창 숨기기용
+    window.scrollTo(0, 1);
+};
